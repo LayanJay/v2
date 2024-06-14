0437aae29acf8c19a52b3336002fe8a807d67e81
LayanJay <layanjayasinghe2@gmail.com>
2024-06-15 01:45:15 +0530

    chore: Update MovingBorderCard component with adjusted z-index for layers

--- src/providers/next-theme-provider.tsx
+++ src/providers/next-theme-provider.tsx
@@ -1,7 +1,5 @@
-"use client";
-
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
 
 export function ThemeProvider({ children, ...props }: Readonly<ThemeProviderProps>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
